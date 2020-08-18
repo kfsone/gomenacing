7a3a8cdc215ac76823b576796d36aaee5ad4328e
Oliver 'kfsone' Smith <oliver@kfs.org>
2020-08-18 08:59:07 -0700

    Initial commodity functions

--- commodity.go
+++ commodity.go
@@ -1,41 +1,52 @@
 package main
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+	"github.com/tidwall/gjson"
+)
+
+// Commodity is a representation of a tradeable item.
+type Commodity struct {
+	DbEntity
+	CategoryId EntityID // Which category the commodity is part of.
+	AvgPrice   int64    // Average market price.
+	FDevId     FDevID   // FrontierDev internal ID.
+}
 
 // MissingCategory is an error raised when attempting to create a commodity without a category.
 var MissingCategory = errors.New("called NewCommodity with nil category")
 
 // Fields used to describe commodity in eddb.
 //var commodityFields = []string{"id", "name", "category.id", "category.name", "is_non_marketable", "average_price", "ed_id"}
-
-// Commodity is a representation of a tradeable item.
-type Commodity struct {
-	DbEntity
-	Category *Category // Which category the commodity is part of.
-	FullName string    // The UI name of the product.
-	AvgPrice int       // Average market price.
-	FDevId   FDevID    // FrontierDev internal ID.
+func NewCommodityFromJson(json []gjson.Result) (*Commodity, error) {
+	id, name, categoryId := json[0].Int(), json[1].String(), json[2].Int()
+	nonMarketable, avgPrice, fdevId := json[3].Bool(), json[4].Int(), json[5].Int()
+	if nonMarketable {
+		return nil, fmt.Errorf("non-marketable item included: %s (#%d)", name, id)
+	}
+	item, err := NewCommodity(id, name, categoryId, avgPrice, fdevId)
+	return item, err
 }
 
 // NewCommodity constructs a Commodity instance based on the input parameters.
-func NewCommodity(id EntityID, dbName string, category *Category, fullName string, avgPrice int, fdevId FDevID) (*Commodity, error) {
-	if category == nil {
-		return nil, MissingCategory
+func NewCommodity(id int64, dbName string, categoryId int64, avgPrice int64, fdevId int64) (*Commodity, error) {
+	dbEntity, err := NewDbEntity(id, dbName)
+	if err != nil {
+		return nil, err
+	}
+	if categoryId <= 0 || categoryId >= 1 << 32 {
+		return nil, fmt.Errorf("invalid commodity category id: %d", categoryId)
 	}
 	return &Commodity{
-		DbEntity: DbEntity{id, dbName},
-		Category: category,
-		FullName: fullName,
-		AvgPrice: avgPrice,
-		FDevId:   fdevId,
+		DbEntity:   dbEntity,
+		CategoryId: EntityID(categoryId),
+		AvgPrice:   avgPrice,
+		FDevId:     FDevID(fdevId),
 	}, nil
 }
 
 // Name is the user-facing name adjusted for level of detail.
-func (i Commodity) Name(detail int) string {
-	if detail > 0 {
-		return i.FullName
-	} else {
-		return i.DbName
-	}
+func (i Commodity) Name() string {
+	return i.DbName
 }
--- commodity_test.go
+++ commodity_test.go
@@ -7,34 +7,28 @@ import (
 )
 
 func TestCommodity_Name(t *testing.T) {
-	category := NewCategory(1001, "Things")
-	commodity, err := NewCommodity(2, "a thing", &category, "a genuine thing", 10, 111)
+	commodity, err := NewCommodity(2, "a thing", 3, 10, 111)
 	assert.Nil(t, err)
 	assert.NotNil(t, commodity)
-	assert.Equal(t, commodity.DbName, commodity.Name(-1))
-	assert.Equal(t, commodity.DbName, commodity.Name(0))
-	assert.Equal(t, commodity.FullName, commodity.Name(1))
-	assert.Equal(t, commodity.FullName, commodity.Name(2))
-	assert.Equal(t, commodity.FullName, commodity.Name(999))
+	assert.Equal(t, commodity.DbName, commodity.Name())
 }
 
 func TestNewCommodity(t *testing.T) {
 	t.Run("NewCommodity", func(t *testing.T) {
-		category := NewCategory(1010, "Things")
-		commodity, err := NewCommodity(2222, "A Thing", &category, "A Genuine Thing", 10, 123)
+		commodity, err := NewCommodity(2222, "A Thing", 7, 10, 123)
 		assert.Nil(t, err)
 		assert.NotNil(t, commodity)
 		assert.Equal(t, EntityID(2222), commodity.Id)
 		assert.Equal(t, "A Thing", commodity.DbName)
-		assert.Equal(t, &category, commodity.Category)
-		assert.Equal(t, "A Genuine Thing", commodity.FullName)
-		assert.Equal(t, 10, commodity.AvgPrice)
+		assert.Equal(t, EntityID(7), commodity.CategoryId)
+		assert.Equal(t, int64(10), commodity.AvgPrice)
 		assert.Equal(t, FDevID(123), commodity.FDevId)
 	})
 
 	t.Run("NewCommodity rejects nil category", func(t *testing.T) {
-		commodity, err := NewCommodity(2222, "A Thing", nil, "A Genuine Thing", 10, 123)
-		assert.Nil(t, commodity)
-		assert.Equal(t, MissingCategory, err)
+		commodity, err := NewCommodity(2222, "A Thing", 0, 10, 123)
+		if assert.Nil(t, commodity) && assert.Error(t, err) {
+			assert.Equal(t, "invalid commodity category id: 0", err.Error())
+		}
 	})
 }
